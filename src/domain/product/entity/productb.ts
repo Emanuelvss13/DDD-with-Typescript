2c6fb712b7f08b190afcfdfcef3c39ee08892311
Emanuelvss13 <emanuelsouza2002@gmail.com>
2023-01-20 11:54:10 -0300

    feat: product factory

--- src/domain/product/entity/productb.ts
+++ src/domain/product/entity/productb.ts
@@ -1,6 +1,6 @@
 import { IProduct } from "./product.interface";
 
-export class Product implements IProduct {
+export class ProductB implements IProduct {
   private _id: string;
   private _name: string;
   private _price: number;
@@ -21,7 +21,7 @@ export class Product implements IProduct {
   }
 
   get price(): number {
-    return this._price;
+    return this._price * 2;
   }
 
   validate(): boolean {
